edafa1f2152393b18409909247d0628746b247fa
turbou <turbou@i.softbank.jp>
2020-09-10 00:21:01 +0900

    update.

---
+++ agent_download.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -X GET https://eval.contrastsecurity.com/Contrast/api/ng/442311fd-c9d6-44a9-a00b-2b03db2d816c/agents/default/JAVA -H 'Authorization: dHVyYm91QGkuc29mdGJhbmsuanA6TDBGUUZZN1JGRVBOQUI4RQ==' -H 'API-Key: EFhK6pIuD6mh5RX6YQ2iMOOavh9Mc52u' -H 'Accept: application/json' -OJ
