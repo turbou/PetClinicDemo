549fa6eeabc8ad4b206cf1d3c55ae96b1b8b0e79
turbou <turbou@i.softbank.jp>
2020-09-11 01:24:56 +0900

    update.

--- check_vul.sh
+++ check_vul.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-count=`curl -X GET "https://eval.contrastsecurity.com/Contrast/api/ng/442311fd-c9d6-44a9-a00b-2b03db2d816c/traces/f17571b8-eb60-4767-81c7-230c6576fd47/?expand=skip_links" \
+count=`curl -s -X GET "https://eval.contrastsecurity.com/Contrast/api/ng/442311fd-c9d6-44a9-a00b-2b03db2d816c/traces/f17571b8-eb60-4767-81c7-230c6576fd47/ids?severities=CRITICAL,HIGH,MEDIUM" \
         -H "Authorization: dHVyYm91QGkuc29mdGJhbmsuanA6TDBGUUZZN1JGRVBOQUI4RQ==" \
         -H "API-Key: EFhK6pIuD6mh5RX6YQ2iMOOavh9Mc52u" \
-        | jq '[.traces[] | select(.default_severity | test("CRITICAL|HIGH")) | .uuid] | length'`
+        | jq '[.traces[]] | length'`
 echo $count
+if [ $count -gt 0 ]; then
+  exit 1
+fi
 
 exit 0
 
