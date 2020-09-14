d380f291a1702a49df97978983b6f84200241982
turbou <turbou@i.softbank.jp>
2020-09-14 23:31:32 +0900

    build number support.

--- check_vul.sh
+++ check_vul.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-count=`curl -s -X GET "https://eval.contrastsecurity.com/Contrast/api/ng/442311fd-c9d6-44a9-a00b-2b03db2d816c/traces/f17571b8-eb60-4767-81c7-230c6576fd47/ids?severities=CRITICAL" \
+APP_VERSION="CODEBUILD_TEST_$1"
+ORG_ID="442311fd-c9d6-44a9-a00b-2b03db2d816c"
+APP_ID="f17571b8-eb60-4767-81c7-230c6576fd47"
+count=`curl -s -X GET "https://eval.contrastsecurity.com/Contrast/api/ng/${ORG_ID}/traces/${APP_ID}/ids?appVersionTags=${APP_VERSION}&severities=CRITICAL" \
         -H "Authorization: dHVyYm91QGkuc29mdGJhbmsuanA6TDBGUUZZN1JGRVBOQUI4RQ==" \
         -H "API-Key: EFhK6pIuD6mh5RX6YQ2iMOOavh9Mc52u" \
         | jq '[.traces[]] | length'`
--- startup_8081_for_test.sh
+++ startup_8081_for_test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+
+APP_VERSION="CODEBUILD_TEST_$1"
 java -javaagent:./contrast.jar \
 -Dcontrast.server.environment=development \
 -Dcontrast.server.name=ec2_CentOS7 \
 -Dcontrast.agent.java.standalone_app_name=PetClinic_8081 \
--Dcontrast.application.version=v8081 \
+-Dcontrast.application.version=$APP_VERSION \
 -Dcontrast.agent.contrast_working_dir=/tmp/contrast-8081/ \
 -Dcontrast.agent.logger.level=INFO \
 -Dcontrast.agent.polling.app_activity_ms=3000 \
