4a4cd70831fa32201b07be98b0c19e8fa1c6f519
turbou <turbou@i.softbank.jp>
2020-09-15 09:35:04 +0900

    update.

--- startup_8081.sh
+++ startup_8081.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-java -javaagent:/tmp/contrast.jar \
--Dcontrast.server.environment=development \
--Dcontrast.server.name=ec2_CentOS7 \
--Dcontrast.agent.java.standalone_app_name=PetClinic_8081 \
--Dcontrast.application.version=v8081 \
--Dcontrast.agent.contrast_working_dir=/tmp/contrast-8081/ \
--Dcontrast.agent.logger.level=INFO \
--Dcontrast.agent.polling.app_activity_ms=3000 \
--Dcontrast.agent.polling.server_activity_ms=3000 \
--Dcontrast.api.timeout_ms=1000 \
--jar /tmp/spring-petclinic-1.5.1.jar --server.port=8081
+java -jar /tmp/spring-petclinic-1.5.1.jar --server.port=8081
