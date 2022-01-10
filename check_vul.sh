301760b131c3e99d8b43797fd53b3d832d5b80b3
turbou <turbou@i.softbank.jp>
2022-01-10 15:44:45 +0900

    update.

--- check_vul.sh
+++ check_vul.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 APP_VERSION="CODEBUILD_TEST_$1"
-ORG_ID="442311fd-c9d6-44a9-a00b-2b03db2d816c"
-APP_ID="f17571b8-eb60-4767-81c7-230c6576fd47"
+ORG_ID="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+APP_ID="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
 count=`curl -s -X GET "https://eval.contrastsecurity.com/Contrast/api/ng/${ORG_ID}/traces/${APP_ID}/ids?appVersionTags=${APP_VERSION}&severities=CRITICAL" \
-        -H "Authorization: dHVyYm91QGkuc29mdGJhbmsuanA6TDBGUUZZN1JGRVBOQUI4RQ==" \
-        -H "API-Key: EFhK6pIuD6mh5RX6YQ2iMOOavh9Mc52u" \
+        -H "Authorization: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX==" \
+        -H "API-Key: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" \
         | jq '[.traces[]] | length'`
 echo $count
 if [ $count -gt 0 ]; then
