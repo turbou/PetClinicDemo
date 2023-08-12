52ed0cb8a02bca0d22ad503699948f2b97798928
turbou <turbou@i.softbank.jp>
2023-08-12 11:08:09 +0900

    Update startup_8001_gitlab_pipeline.sh

--- startup_8001_gitlab_pipeline.sh
+++ startup_8001_gitlab_pipeline.sh
@@ -3,13 +3,13 @@ if [ $# -eq 1 ]; then
     APP_VERSION=${1}
 fi
 java -javaagent:../target/contrast-agent-5.2.2.jar \
--Dcontrast.api.url=https://eval.contrastsecurity.com/Contrast \
--Dcontrast.api.api_key=EFhK6pIuD6mh5RX6YQ2iMOOavh9Mc52u \
--Dcontrast.api.service_key=L3EWY1E2OKWVZ4G0 \
--Dcontrast.api.user_name=agent_442311fd-c9d6-44a9-a00b-2b03db2d816c@Tabocom \
--Dcontrast.server.environment=development \
--Dcontrast.server.name=Gitlab-Pipeline \
--Dcontrast.agent.java.standalone_app_name=PetClinic_8001_GitlabDemo \
+-Dcontrast.api.url=${CONTRAST_URL} \
+-Dcontrast.api.api_key=${API_KEY} \
+-Dcontrast.api.service_key=${SERVICE_KEY} \
+-Dcontrast.api.user_name=${USER_NAME} \
+-Dcontrast.server.environment=${ENVIRONMENT} \
+-Dcontrast.server.name=${SERVER_NAME} \
+-Dcontrast.agent.java.standalone_app_name=${APP_NAME} \
 -Dcontrast.application.version=${APP_VERSION} \
 -Dcontrast.override.appversion=${APP_VERSION} \
 -Dcontrast.agent.contrast_working_dir=contrast-8001/ \
